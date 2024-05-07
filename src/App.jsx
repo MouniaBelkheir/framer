26c59d3d15a06e05637259f7163ee2f549989718
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-05-07 23:38:29 +0100

    added fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { motion } from "framer-motion";
-import { Button } from "@material-tailwind/react";
+
 function App() {
 	return (
 		<div className="flex flex-col gap-10 overflow-x-hidden">
 			<motion.section>
-				<h1 className="text-rose-500">hello</h1>
-				<Button>Button</Button>
+				<h1 className="text-rose-500 font-petit ">hello</h1>
+				
 			</motion.section>
 		</div>
 	);
