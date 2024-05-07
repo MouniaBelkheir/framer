b497583a14a961fdb4f2a619b51a6d2f27133b96
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-05-07 22:43:22 +0100

    installing dependencies material-tailwind/react and react scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { motion } from "framer-motion";
+import { Button } from "@material-tailwind/react";
 function App() {
-	return <h1 className="text-3xl font-bold text-rose-500">Hello</h1>;
+	return (
+		<div className="flex flex-col gap-10 overflow-x-hidden">
+			<motion.section>
+				<h1 className="text-rose-500">hello</h1>
+				<Button>Button</Button>
+			</motion.section>
+		</div>
+	);
 }
 
 export default App;
